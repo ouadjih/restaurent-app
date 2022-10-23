39993a5a9f9bdaafae1f1bb9cfb6115d04d50ef1
ouadjih <47463757+ouadjih@users.noreply.github.com>
2022-10-23 13:47:42 +0200

    add create item component (form)

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -1,8 +1,157 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
+import {MdAttachMoney, MdCloudUpload, MdFastfood, MdFoodBank, MdMoney} from 'react-icons/md'
+import { categories } from '../utils/data'
+import Loader from './Loader'
+import { storage } from '../firebase.config'
+import {ref} from 'firebase/database'
+import {uploadBytesResumable } from'firebase/storage'
 
  const CreateContainer = () => {
+  const [title,setTitle] =useState('')
+  const [calories,setCalories] =useState('')
+  const [price,setPrice] =useState('')
+  const [category,setCategory] =useState(null)
+  const [imageAsset ,setImageAsset] = useState(null)
+  const [fields,setFields] =useState(false)
+  const [alertStatus, setAlertStatus]=useState("danger")
+  const [msg,setMsg]=useState(null)
+  const [isLoading,setIsLoading]=useState(false)
+
+  const uploadimage = (e)=>{
+    setIsLoading(true)
+    const imageFile = e.target.files[0]
+    const storegeRef = ref(storage,`images/${Date.now()}-${imageFile.name}`)
+    const uploadTask = uploadBytesResumable(storegeRef , imageFile);
+    uploadTask.on('state_changed',(snapshot)=>{
+      const uploadProgress = (snapshot.bytesTransferred /snapshot.totalBytes) * 100
+    }, (error)=>{
+      console.log(error)
+      setFields(true)
+      setMsg("error while uploading : Try again")
+    },()=>{})
+  }
+  const deleteImage = ()=>{
+
+  }
+ 
+  const saveDetails= ()=>{}
   return (
-    <div>CreateContainer</div>
+    <div className='w-full min-h-screen flex items-center justify-center'>
+      <div 
+      className="w-[90%] md:w-[75%] border border-gray-400 
+      rounded-lg p-4 flex flex-col items-center justify-center gap-4">
+      {fields && (
+        <motion.p 
+        initial={{opacity:0}}
+        animate={{opacity:1}}
+        exit={{opacity:0}}
+          className={`w-full p-2 rounded-lg text-center text-lg font-semibold ${alertStatus === 'danger' ?
+        'bg-red-400 text-red-800':'bg-emerald-400 text-emerald-600'}`}>
+          {msg}
+        </motion.p>
+      )}
+      <div className="w-full py-2 border-b border-gray-300 flex items-center gap-2 ">
+        <MdFastfood className="text-xl text-gray-700"/>
+        <input 
+        type="text" 
+        required 
+        value={title} 
+        placeholder="give me title..." 
+        className='w-full h-full text-lg bg-transparent  outline-none 
+        border-none placeholder:text-gray-400 text-textColor'
+        onChange={e=> setTitle(e.target.value)}/>
+      </div>
+      <div className="w-full">
+        <select 
+        onChange={e=> setCategory(e.target.value)}
+        className="outline-none w-full text-base border-b-2
+         border-gray-200 p-2 rounded-md cursor-pointer"
+        >
+          <option value="other" className='bg-white'>
+            Select Category
+          </option>
+            {categories && categories.map(item =>
+              <option key={item.id} className="text-base border-0 
+               outline-none capitalize bg-white text-textColor"
+               value={item.urlParamName}>
+                {item.name}
+              </option>
+            )}
+
+        </select>
+      </div>
+      <div
+       className="group flex justify-center items-center 
+      flex-col border-2 border-dotted border-gray-500 w-full
+       h-225 md:h-420 cursor-pointer rounded-lg">
+      {isLoading?<Loader/>:
+      <>
+        {!imageAsset ?
+        
+          <label className='w-full h-full flex flex-col items-center justify-center cursor-pointer'>
+            <div className="w-full h-full flex flex-col items-center justify-center gap-2">
+              <MdCloudUpload 
+                className='text-gray-500 text-3xl hover:text-gray-700'/> 
+                <p className='text-gray-500 hover:text-gray-700'>Click here to upload image</p>
+            </div>
+            <input 
+              type="file"  
+              name="uploadimage" 
+              accept='image/*'
+              onChange={uploadimage} 
+              className="w-0 h-0"/>
+          </label>
+        :
+        
+        <div className="relative h-full">
+          <img 
+            src={imageAsset} 
+            alt="uploaded"
+            className='h-full w-full object-cover'/>
+          <button 
+            type="button"
+            className='absolute bottom-3 right-3 p-3 
+            rounded-full bg-red-500 text-xl cursor-pointer
+            outline-none hover:shadow-md duration-500 transition-all 
+            ease-in-out' 
+           onClick={deleteImage}><mdDelete className="text-white"/></button>
+        </div>
+        }  
+      </>}
+       </div>
+       <div className="w-full flex flex-col md:flex-row items-center gap-3">
+        <div className="w-full py-2 border-b border-gray-300  flex items-center gap-2">
+          <MdFoodBank className='text-gray-700 text-3xl'/>
+          <input 
+            type="text"
+            required
+            value={calories}
+            onChange={(e)=>{setCalories(e.target.value)}}
+            placeholder='Calories'
+            className='w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400'/>
+        </div>
+        <div className="w-full py-2 border-b border-gray-300  flex items-center gap-2">
+          <MdAttachMoney className='text-gray-700 text-3xl'/>
+          <input 
+            type="text"
+            required
+            value={price}
+            onChange={(e)=>{setPrice(e.target.value)}}
+            placeholder='Price'
+            className='w-full h-full text-lg bg-transparent outline-none border-none placeholder:text-gray-400'/>
+        </div>
+       </div>
+       <div className='flex items-center w-full '>
+        <button 
+          type='button'
+          className='ml-0 md:ml-auto w-full md:w-auto 
+          border-none outline-none bg-emerald-500 
+          px-12 py-2 rounded-lg text-lg text-white font-semibold'
+          onClick={saveDetails}>SAVE</button>
+       </div>
+      </div>
+    </div>
   )
 }
 
--- src/utils/data.js
+++ src/utils/data.js
@@ -8,7 +8,7 @@ export const heroData = [
       name:'Icecream',
       desc:"Chocolate & vanilla",
       price: '5.25',
-      img:`../img/i1.png`
+      img:{i1}
     },
     {
         id:2,
@@ -31,4 +31,41 @@ export const heroData = [
         price: '5.25',
         img:{fi1}
       },
+  ]
+ export const categories =[
+    {
+      id:1,
+      name:'Chicken',
+      urlParamName:'chicken'
+    },
+    {
+      id:2,
+      name:'Curry',
+      urlParamName:'curry'
+    },
+    {
+      id:3,
+      name:'Rice',
+      urlParamName:'rice'
+    },
+    {
+      id:4,
+      name:'Fish',
+      urlParamName:'fish'
+    },
+    {
+      id:5,
+      name:'Fruits',
+      urlParamName:'fruits'
+    },
+    {
+      id:6,
+      name:'Icecreams',
+      urlParamName:'icecreams'
+    },
+    {
+      id:7,
+      name:'Soft Drinks',
+      urlParamName:'drinks'
+    },
   ]
