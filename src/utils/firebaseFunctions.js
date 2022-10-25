8484b53b101c8444b5e9f92f98c79406386ff2c9
ouadjih <47463757+ouadjih@users.noreply.github.com>
2022-10-25 12:15:18 +0200

    last commit

--- src/utils/firebaseFunctions.js
+++ src/utils/firebaseFunctions.js
@@ -1,22 +1,25 @@
-//saving new item
-
-import { firestore } from "../firebase.config"
-import {doc,setDoc,getDocs,query,collection,orderBy} from "firebase/firestore"
-export const saveItem = async (data)=>{
-    await setDoc(
-        doc(firestore,'foodItems',`${Date.now()}`),
-        data,
-        {merge:true}
-    )
-}
-
-export const getAllFoodItems = async ()=>{
+import {
+    collection,
+    doc,
+    getDocs,
+    orderBy,
+    query,
+    setDoc,
+  } from "firebase/firestore";
+  import { firestore } from "../firebase.config";
+  
+  // Saving new Item
+  export const saveItem = async (data) => {
+    await setDoc(doc(firestore, "foodItems", `${Date.now()}`), data, {
+      merge: true,
+    });
+  };
+  
+  // getall food items
+  export const getAllFoodItems = async () => {
     const items = await getDocs(
-        query(
-            collection(firestore,"foodItems"),
-            orderBy("id","desc")
-        )
-    )
-    return items.docs.map((docs)=>docs.data())
-}
-
+      query(collection(firestore, "foodItems"), orderBy("id", "desc"))
+    );
+  
+    return items.docs.map((doc) => doc.data());
+  };
